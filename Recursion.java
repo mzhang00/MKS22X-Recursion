2f3356c61fa16f8711865565b377bbf0ad264ab4
Michael Zhang <mzhang00@stuy.edu>
2019-02-03 21:12:05 -0500

    Update add method with working code

--- Recursion.java
+++ Recursion.java
@@ -1,6 +1,6 @@
 public class Recursion{
     public static double sqrt(double n, double tolerance){
-        return 0.0;
+        return NewtonHelper(n, 1, tolerance);
     }
     public static double NewtonHelper(double n, double guess, double tolerance){
         return 0.0;
