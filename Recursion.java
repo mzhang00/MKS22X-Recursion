1712dd68ef90481a41654134d3ecbb8d2fbe8932
Michael Zhang <mzhang00@stuy.edu>
2019-02-03 21:11:16 -0500

    Add Newton's root helper method

--- Recursion.java
+++ Recursion.java
@@ -2,4 +2,7 @@ public class Recursion{
     public static double sqrt(double n, double tolerance){
         return 0.0;
     }
+    public static double NewtonHelper(double n, double guess, double tolerance){
+        return 0.0;
+    }
 }
